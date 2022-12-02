0b1e39eed04905b4c607ace1e7e8b9e3dad57145
Tony <tonyyunyang@outlook.com>
2022-12-02 12:42:49 +0100

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,14 @@ fn main() {
     // println!("----------------------------------------------------------------");
 
     // Day 2
-    
+    let file = fs::read_to_string("quiz2_input.txt").expect("Error, file not read");
+    let points = get_total_points(file.clone());
 
+    println!("The total point is {}", points);
 
+    let second_points = get_total_points2(file);
+
+    println!("The total point is {}", second_points);
 }
 
 // Day 1
@@ -40,4 +45,95 @@ fn get_the_elf(content: String) -> (u64, u64, u64) {
         calories.pop().unwrap(),
         calories.pop().unwrap(),
     )
+}
+
+// Day 2
+fn get_total_points(content: String) -> i32 {
+    // let mut total: i32 = 0;
+
+    let mut sum = content.lines().into_iter().fold(0, |mut total, line| {
+        let decision_string = line.split(" ").collect::<Vec<&str>>();
+        let elf_decision = decision_string[0];
+        let my_decision = decision_string[1];
+        match elf_decision {
+            "A" => {
+                if my_decision == "X" {
+                    total += (1 + 3);
+                } else if my_decision == "Y" {
+                    total += (2 + 6);
+                }
+                else if my_decision == "Z"{
+                    total += (3);
+                }
+            }
+            "B" => {
+                if my_decision == "X" {
+                    total += (1);
+                } else if my_decision == "Y" {
+                    total += (2 + 3);
+                }
+                else if my_decision == "Z"{
+                    total += (3 + 6);
+                }
+            }
+            "C" => {
+                if my_decision == "X" {
+                    total += (1 + 6);
+                } else if my_decision == "Y" {
+                    total += (2);
+                }
+                else if my_decision == "Z"{
+                    total += (3 + 3);
+                }
+            }
+            _ => {}
+        }
+        total
+    });
+    sum
+}
+
+fn get_total_points2(content: String) -> i32 {
+    // let mut total: i32 = 0;
+
+    let mut sum = content.lines().into_iter().fold(0, |mut total, line| {
+        let decision_string = line.split(" ").collect::<Vec<&str>>();
+        let elf_decision = decision_string[0];
+        let my_decision = decision_string[1];
+        match elf_decision {
+            "A" => {
+                if my_decision == "X" {
+                    total += (3 + 0);
+                } else if my_decision == "Y" {
+                    total += (1 + 3);
+                }
+                else if my_decision == "Z"{
+                    total += (2 + 6);
+                }
+            }
+            "B" => {
+                if my_decision == "X" {
+                    total += (1 + 0);
+                } else if my_decision == "Y" {
+                    total += (2 + 3);
+                }
+                else if my_decision == "Z"{
+                    total += (3 + 6);
+                }
+            }
+            "C" => {
+                if my_decision == "X" {
+                    total += (2 + 0);
+                } else if my_decision == "Y" {
+                    total += (3 + 3);
+                }
+                else if my_decision == "Z"{
+                    total += (1 + 6);
+                }
+            }
+            _ => {}
+        }
+        total
+    });
+    sum
 }
