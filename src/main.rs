487b09dde6be55663be6f99c2bf439b79df8d9d5
Tony <tonyyunyang@outlook.com>
2022-12-04 15:22:31 +0100

    cargo clippy autofix

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::{
     fs::{self, File},
     io::{BufRead, BufReader},
-    num,
     ops::Div,
 };
 
@@ -19,40 +18,39 @@ fn main() {
     let points = get_total_points(file.clone());
     println!("Day 2 ----------------------------------------------------------");
 
-    println!("The total point is {}", points);
+    println!("The total point is {points}");
 
     let second_points = get_total_points2(file);
 
-    println!("The total point is {}", second_points);
+    println!("The total point is {second_points}");
 
     // Day 3
     println!("Day 3 ----------------------------------------------------------");
 
     let file = fs::read_to_string("quiz3_input.txt").expect("Error, file not here");
-    let each_match = get_each_char(file.clone());
+    let each_match = get_each_char(file);
 
-    let total_sum = get_total_sum(each_match.clone());
+    let total_sum = get_total_sum(each_match);
 
-    println!("The total sum of today is {}", total_sum);
+    println!("The total sum of today is {total_sum}");
 
     let second_match = get_each_char2();
 
-    let total_sum2 = get_total_sum(second_match.clone());
+    let total_sum2 = get_total_sum(second_match);
 
-    println!("The second total sum of today is {}", total_sum2);
+    println!("The second total sum of today is {total_sum2}");
 
     // Day 4
     println!("Day 4 ----------------------------------------------------------");
 
     let file = fs::read_to_string("quiz4_input.txt").expect("Error, file not here");
     let fully_overlaps = camp_cleanup(file.clone());
-    
-    println!("The number of  fully overlaps group is {}", fully_overlaps);
 
-    let partly_overlaps = camp_cleanup2(file.clone());
+    println!("The number of  fully overlaps group is {fully_overlaps}");
 
-    println!("The number of partly overlaps group is {}", partly_overlaps);
+    let partly_overlaps = camp_cleanup2(file);
 
+    println!("The number of partly overlaps group is {partly_overlaps}");
 }
 
 // Day 1
@@ -86,35 +84,35 @@ fn get_total_points(content: String) -> i32 {
     // let mut total: i32 = 0;
 
     let sum = content.lines().into_iter().fold(0, |mut total, line| {
-        let decision_string = line.split(" ").collect::<Vec<&str>>();
+        let decision_string = line.split(' ').collect::<Vec<&str>>();
         let elf_decision = decision_string[0];
         let my_decision = decision_string[1];
         match elf_decision {
             "A" => {
                 if my_decision == "X" {
-                    total += (1 + 3);
+                    total += 1 + 3;
                 } else if my_decision == "Y" {
-                    total += (2 + 6);
+                    total += 2 + 6;
                 } else if my_decision == "Z" {
-                    total += (3);
+                    total += 3;
                 }
             }
             "B" => {
                 if my_decision == "X" {
-                    total += (1);
+                    total += 1;
                 } else if my_decision == "Y" {
-                    total += (2 + 3);
+                    total += 2 + 3;
                 } else if my_decision == "Z" {
-                    total += (3 + 6);
+                    total += 3 + 6;
                 }
             }
             "C" => {
                 if my_decision == "X" {
-                    total += (1 + 6);
+                    total += 1 + 6;
                 } else if my_decision == "Y" {
-                    total += (2);
+                    total += 2;
                 } else if my_decision == "Z" {
-                    total += (3 + 3);
+                    total += 3 + 3;
                 }
             }
             _ => {}
@@ -127,36 +125,36 @@ fn get_total_points(content: String) -> i32 {
 fn get_total_points2(content: String) -> i32 {
     // let mut total: i32 = 0;
 
-    let mut sum = content.lines().into_iter().fold(0, |mut total, line| {
-        let decision_string = line.split(" ").collect::<Vec<&str>>();
+    let sum = content.lines().into_iter().fold(0, |mut total, line| {
+        let decision_string = line.split(' ').collect::<Vec<&str>>();
         let elf_decision = decision_string[0];
         let my_decision = decision_string[1];
         match elf_decision {
             "A" => {
                 if my_decision == "X" {
-                    total += (3 + 0);
+                    total += 3;
                 } else if my_decision == "Y" {
-                    total += (1 + 3);
+                    total += 1 + 3;
                 } else if my_decision == "Z" {
-                    total += (2 + 6);
+                    total += 2 + 6;
                 }
             }
             "B" => {
                 if my_decision == "X" {
-                    total += (1 + 0);
+                    total += 1;
                 } else if my_decision == "Y" {
-                    total += (2 + 3);
+                    total += 2 + 3;
                 } else if my_decision == "Z" {
-                    total += (3 + 6);
+                    total += 3 + 6;
                 }
             }
             "C" => {
                 if my_decision == "X" {
-                    total += (2 + 0);
+                    total += 2;
                 } else if my_decision == "Y" {
-                    total += (3 + 3);
+                    total += 3 + 3;
                 } else if my_decision == "Z" {
-                    total += (1 + 6);
+                    total += 1 + 6;
                 }
             }
             _ => {}
@@ -175,7 +173,7 @@ fn get_each_char(content: String) -> Vec<char> {
             let num_of_char = line.len() as u32;
             let half_num_of_char = num_of_char.div(2);
             'outer: for i in 0..half_num_of_char {
-                let first_each_char = line.clone().chars().nth(i as usize).unwrap();
+                let first_each_char = line.chars().nth(i as usize).unwrap();
                 for j in half_num_of_char..num_of_char {
                     if line.chars().nth(j as usize).unwrap() == first_each_char {
                         all_chars.push(first_each_char);
@@ -199,10 +197,10 @@ fn get_total_sum(chars: Vec<char>) -> u32 {
     // test.push('s');
     for i in chars {
         let temp = i as u32;
-        if (temp >= 65) && (temp <= 90) {
-            sum += (temp - 38);
-        } else if (temp >= 97) && (temp <= 122) {
-            sum += (temp - 96);
+        if (65..=90).contains(&temp) {
+            sum += temp - 38;
+        } else if (97..=122).contains(&temp) {
+            sum += temp - 96;
         }
     }
     sum
@@ -239,18 +237,18 @@ fn get_each_char2() -> Vec<char> {
 
 // Day 4
 fn camp_cleanup(content: String) -> u32 {
-    let sum = content.lines().into_iter().fold(0, |mut total, line|{
-        let each_line = line.split(",").collect::<Vec<&str>>();
-        // for i in each_line {
-        //     println!("{}", i);
-        // }
-        let first_group = each_line[0].split("-").collect::<Vec<&str>>();
-        let first_range:Vec<u32> = first_group.into_iter().map(|a| a.trim().parse::<u32>().expect("Not a number")).collect();
-        // for i in first_range {
-        //     println!("{}", i);
-        // }
-        let second_group = each_line[1].split("-").collect::<Vec<&str>>();
-        let second_range:Vec<u32> = second_group.into_iter().map(|a| a.trim().parse::<u32>().expect("Not a number")).collect();
+    let sum = content.lines().into_iter().fold(0, |mut total, line| {
+        let each_line = line.split(',').collect::<Vec<&str>>();
+        let first_group = each_line[0].split('-').collect::<Vec<&str>>();
+        let first_range: Vec<u32> = first_group
+            .into_iter()
+            .map(|a| a.trim().parse::<u32>().expect("Not a number"))
+            .collect();
+        let second_group = each_line[1].split('-').collect::<Vec<&str>>();
+        let second_range: Vec<u32> = second_group
+            .into_iter()
+            .map(|a| a.trim().parse::<u32>().expect("Not a number"))
+            .collect();
 
         let mut flag = true;
 
@@ -269,23 +267,21 @@ fn camp_cleanup(content: String) -> u32 {
 }
 
 fn camp_cleanup2(content: String) -> u32 {
-    let sum = content.lines().into_iter().fold(0, |mut total, line|{
-        let each_line = line.split(",").collect::<Vec<&str>>();
-        // for i in each_line {
-        //     println!("{}", i);
-        // }
-        let first_group = each_line[0].split("-").collect::<Vec<&str>>();
-        let first_range:Vec<u32> = first_group.into_iter().map(|a| a.trim().parse::<u32>().expect("Not a number")).collect();
-        // for i in first_range {
-        //     println!("{}", i);
-        // }
-        let second_group = each_line[1].split("-").collect::<Vec<&str>>();
-        let second_range:Vec<u32> = second_group.into_iter().map(|a| a.trim().parse::<u32>().expect("Not a number")).collect();
-
+    let sum = content.lines().into_iter().fold(0, |mut total, line| {
+        let each_line = line.split(',').collect::<Vec<&str>>();
+        let first_group = each_line[0].split('-').collect::<Vec<&str>>();
+        let first_range: Vec<u32> = first_group
+            .into_iter()
+            .map(|a| a.trim().parse::<u32>().expect("Not a number"))
+            .collect();
+        let second_group = each_line[1].split('-').collect::<Vec<&str>>();
+        let second_range: Vec<u32> = second_group
+            .into_iter()
+            .map(|a| a.trim().parse::<u32>().expect("Not a number"))
+            .collect();
         if (first_range[0] <= second_range[1]) && (first_range[1] >= second_range[0]) {
             total += 1;
         }
-
         total
     });
     sum
